3bc86cb9471b211819957069f7eec6ab140c7b7b
Vick Kumar <Vick.Kumar44@gmail.com>
2023-09-13 19:47:49 -0400

    added nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { Navigation } from "./components/navigation/Navigation";
+
 function App() {
   return (
     <>
+      <Navigation title="Mortgage Calculator" />
       <h1>Hello World</h1>
     </>
   );
